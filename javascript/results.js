f55a4b60da471e50335cc4dab81469722200d61d
TheOptic57 <thecalebzhou@gmail.com>
2022-11-27 08:49:40 -0500

    added way to display survey results
    title

--- javascript/results.js
+++ javascript/results.js
@@ -1,3 +1,17 @@
 function GetResults() {
-    document.getElementById("results").innerHTML = "Results Displayed here";
+    let tempmean1 = 20;
+    let temptitle1 = "TEST TITLE 1"
+
+    let tempmean2 = 10;
+    let temptitle2 = "TEST TITLE 2"
+
+    document.getElementById("Copy").innerHTML = "<h4>" + temptitle1  + "</h4>" + "<br>" +  "Mean: " + tempmean1 + "<br>";
+
+    let currentDiv = document.getElementById("Copy");
+    for (var i = 0; i < 5; i++) {
+        let divClone = currentDiv.cloneNode(true);
+        divClone.innerHTML = "<h4>" + temptitle2  + "</h4>" + "<br>" +  "Mean: " + tempmean2;
+        document.getElementById("after").appendChild(divClone);
+    } 
+
 }
