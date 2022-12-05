9e9e278e9d68c0a46c9597d5414e6b88ce30a00f
TheOptic57 <thecalebzhou@gmail.com>
2022-12-05 18:48:01 -0500

    second api for takesurvey
    title

--- javascript/takesurvey.js
+++ javascript/takesurvey.js
@@ -56,12 +56,10 @@ function generateSurvey(surveyid) {
     getSurvey.appendChild(title);
 
 
-    let tmp = {email:email};
+    let tmp = {Sid:surveyid.value};
 	let jsonPayload = JSON.stringify(tmp);
-
 	let url = 'http://localhost/get_takeSurvey.php';
 
-    let SurveyTitles = [];
     let xhr = new XMLHttpRequest();
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
@@ -71,10 +69,48 @@ function generateSurvey(surveyid) {
 				let jsonObject = JSON.parse(xhr.responseText);
 
 				if (jsonObject.status == 'failure') {
-					document.getElementById("nosurvey").innerHTML = "Error no surveys to be taken";
-                    alert(localStorage.getItem("email"))
+					document.getElementById("nosurvey").innerHTML = "Error retrieving survey";
 					return;
 				}
+                //alert(jsonObject.response[0].T2id)
+                for(let i = 0; i < jsonObject.response.length; i++) {
+                    const question = document.createElement('div');
+                    question.innerHTML = "Question: " + jsonObject.response[i].Question + "<br/>";
+                    getSurvey.appendChild(question);
+            
+                    //if statement to check if it is type 2 right now its check for even just to see if it works
+                    if(jsonObject.response[i].T1id === undefined) {
+                        //<textarea rows = "5" cols = "60" name = "Awnser" id="Awnser" style="width: 80%;">
+                        const newInput = document.createElement('textarea');
+                        newInput.setAttribute('name', "Type2");
+                        newInput.setAttribute('rows', "5");
+                        newInput.setAttribute('cols', "60");
+                        getSurvey.appendChild(newInput);
+                    }
+                    else {
+                        /*
+                        <select name="Type2">
+                                        <option value="1">1 </option>
+                                        <option value="2">2</option>
+                                        .
+                                        .
+                                        .
+                                    </select>
+                        */
+                        let newSelect = document.createElement('select');
+                        newSelect.setAttribute('Name', "Type1");
+            
+                        for (let j = 1; j < 6; j++) {
+            
+                            const newOption = document.createElement('option');
+                            const optionText = document.createTextNode(j);
+                            newOption.appendChild(optionText);
+                            newOption.setAttribute('value', j);
+                            newSelect.appendChild(newOption);
+                        }
+                        getSurvey.appendChild(newSelect);
+                    }
+                }
 			}
 		};
 		xhr.send(jsonPayload);
@@ -82,45 +118,5 @@ function generateSurvey(surveyid) {
 	catch (err) {
 		document.getElementById("nosurvey").innerHTML = err.message;
 	}
-
-
-    for(let i = 0; i < 1; i++) {
-        const question = document.createElement('div');
-        question.innerHTML = "Question: " + "stuff" + "<br/>";
-        getSurvey.appendChild(question);
-
-        //if statement to check if it is type 2 right now its check for even just to see if it works
-        if(i % 2 == 0) {
-            //<textarea rows = "5" cols = "60" name = "Awnser" id="Awnser" style="width: 80%;">
-            const newInput = document.createElement('textarea');
-            newInput.setAttribute('name', "Type2");
-            newInput.setAttribute('rows', "5");
-            newInput.setAttribute('cols', "60");
-            getSurvey.appendChild(newInput);
-        }
-        else {
-            /*
-            <select name="Type2">
-                            <option value="1">1 </option>
-                            <option value="2">2</option>
-                            .
-                            .
-                            .
-                        </select>
-            */
-            let newSelect = document.createElement('select');
-            newSelect.setAttribute('Name', "Type1");
-
-            for (let j = 1; j < 6; j++) {
-
-                const newOption = document.createElement('option');
-                const optionText = document.createTextNode(j);
-                newOption.appendChild(optionText);
-                newOption.setAttribute('value', j);
-                newSelect.appendChild(newOption);
-            }
-            getSurvey.appendChild(newSelect);
-        }
-    }
 }
 
