bbae4d835dce1710c4a23e8362968bd80c8b0764
TheOptic57 <thecalebzhou@gmail.com>
2022-12-05 18:17:35 -0500

    first part of takesuvey
    title

--- javascript/takesurvey.js
+++ javascript/takesurvey.js
@@ -1,29 +1,90 @@
 function GetSurveyTitle() {
-    let SurveyTitles = ["Test1", "Test2", "Test3", "Test4"];
-    let options = document.getElementById("SurveyTitle");
-    const select = document.querySelector('select'); 
-    for (var i = 0; i < SurveyTitles.length; i++) {
-        const newOption = document.createElement('option');
-        const optionText = document.createTextNode(SurveyTitles[i]);
-        newOption.appendChild(optionText);
-        newOption.setAttribute('value', SurveyTitles[i]);
-        newOption.setAttribute('onclick', "generateSurvey(this)");
-        select.appendChild(newOption);
-    }
+    let email = localStorage.getItem("email");
+    let tmp = {email:email};
+	let jsonPayload = JSON.stringify(tmp);
+
+	let url = 'http://localhost/get_participatingSurvey.php';
+
+    let SurveyTitles = [];
+    let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try {
+		xhr.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				let jsonObject = JSON.parse(xhr.responseText);
+
+				if (jsonObject.status == 'failure') {
+					document.getElementById("nosurvey").innerHTML = "Error no surveys to be taken";
+                    alert(localStorage.getItem("email"))
+					return;
+				}
+                for(let i = 0; i < jsonObject.response.length; i++) {
+                    SurveyTitles.push(jsonObject.response[i].Title);
+                    //alert(SurveyTitles);
+                }
+                let options = document.getElementById("SurveyTitle");
+                const select = document.querySelector('select'); 
+                for (var i = 0; i < SurveyTitles.length; i++) {
+                    const newOption = document.createElement('option');
+                    const optionText = document.createTextNode(SurveyTitles[i]);
+                    newOption.appendChild(optionText);
+                    localStorage.setItem(jsonObject.response[i].Sid, SurveyTitles[i]);
+                    newOption.setAttribute('value', jsonObject.response[i].Sid);
+                    newOption.setAttribute('onclick', "generateSurvey(this)");
+                    select.appendChild(newOption);
+                }
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch (err) {
+		document.getElementById("nosurvey").innerHTML = err.message;
+	}
 }
 
 var menu = document.getElementById("SurveyTitle");
 
-function generateSurvey(survey) {
+function generateSurvey(surveyid) {
+    //alert(survey.value);
     const getSurvey = document.getElementById('getSurvey');
     while (getSurvey.firstChild) {
         getSurvey.removeChild(getSurvey.lastChild);
     }
     const title = document.createElement('div');
-    title.innerHTML = "SurveyTitle: " + survey.value + "<br/>";
+    title.innerHTML = "SurveyTitle: " + localStorage.getItem(surveyid.value); + "<br/>";
     getSurvey.appendChild(title);
 
-    for(let i = 0; i < 4; i++) {
+
+    let tmp = {email:email};
+	let jsonPayload = JSON.stringify(tmp);
+
+	let url = 'http://localhost/get_takeSurvey.php';
+
+    let SurveyTitles = [];
+    let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try {
+		xhr.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				let jsonObject = JSON.parse(xhr.responseText);
+
+				if (jsonObject.status == 'failure') {
+					document.getElementById("nosurvey").innerHTML = "Error no surveys to be taken";
+                    alert(localStorage.getItem("email"))
+					return;
+				}
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch (err) {
+		document.getElementById("nosurvey").innerHTML = err.message;
+	}
+
+
+    for(let i = 0; i < 1; i++) {
         const question = document.createElement('div');
         question.innerHTML = "Question: " + "stuff" + "<br/>";
         getSurvey.appendChild(question);
